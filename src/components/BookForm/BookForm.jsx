2d80ca41740c24db1c6fea114f8ec9d2e074d2a7
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-02-23 15:19:06 -0600

    new books can be added to the server and displayed on the dom

--- src/components/BookForm/BookForm.jsx
+++ src/components/BookForm/BookForm.jsx
@@ -1,32 +1,44 @@
-import {useState} from 'react';
+import { useState } from 'react';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
 
-function BookForm() {
+function BookForm({fetchBookList}) {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
+
+  const dispatch = useDispatch();
 
   const handleSubmit = event => {
     event.preventDefault();
 
-    console.log(`Adding book`, {title, author});
+    console.log(`Adding book`, { title, author });
 
     // TODO - axios request to server to add book
-
+    axios.post('/books', { title, author })
+      .then(response => {
+        setTitle('');
+        setAuthor('');
+        fetchBookList();
+      }).catch(err => {
+        console.log('error is', err);
+        alert(`ALERT! I love crossfit!💪`)
+      })
   };
 
   return (
     <section>
       <h2>Add Book</h2>
       <form onSubmit={handleSubmit} className="add-book-form">
-        <input 
-          required 
-          placeholder="Title" 
+        <input
+          required
+          placeholder="Title"
           value={title}
           onChange={(event) => setTitle(event.target.value)}
         />
 
-        <input 
-          required 
-          placeholder="Author" 
+        <input
+          required
+          placeholder="Author"
           value={author}
           onChange={(event) => setAuthor(event.target.value)}
         />
